940431c1404cace664d78f819dd8776fb6b389ff
Omkar Mohite <109783366+OmkarMohite505@users.noreply.github.com>
2024-07-22 10:44:29 +0530

    minor chnages done

--- obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc67afb5fec48c9b4c6344fa6ba8025de505eece")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
